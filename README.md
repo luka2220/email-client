29f59a2c1beb5762e060974764e86c583094f5e4
Luka Piplica <piplicaluka64@gmail.com>
2025-08-28 21:57:57 -0400

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+### Email Desktop Client
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <div className="text-3xl text-amber-700">Email Client Running</div>
+    </>
+  );
+}
+
+export default App;
