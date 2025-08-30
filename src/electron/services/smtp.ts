4182a02c6706594e145af3ffe74f02ce8dc67ac6
Luka Piplica <piplicaluka64@gmail.com>
2025-08-29 22:34:46 -0400

    IMAP greeting message parsing can connection stream parsing

--- src/electron/services/smtp.ts
+++ src/electron/services/smtp.ts
@@ -6,26 +6,54 @@
 
 // IMAP is used to receive/fetch emails
 
-import net from "node:net";
 import tls from "node:tls";
+import dotenv from "dotenv";
 
-// const client = new net.Socket();
-// const unSecureSocket = client.connect(
-//   { port: 993, host: "imap.gmail.com" },
-//   () => {}
-// );
+dotenv.config();
+
+type GreetResponse = "OK" | "PREAUTH" | "BYE";
+
+const gmailUserName = process.env.TEST_GMAIL_USERNAME;
+const gmailPassword = process.env.TEST_GMAIL_PASSWORD;
 
 const socket = tls.connect(
   { host: "imap.gmail.com", port: 993, servername: "imap.gmail.com" },
-  () => console.log("secure connection established")
+  () => {
+    console.log("secure connection established");
+  }
 );
-// const securedConnection = tls.connect({ socket: unSecureSocket }, () => {
-//   console.log("Google IMAP connected with TLS");
-// });
 
-socket.on("data", (data) => {
-  console.log(`IMAP data over TLS -> ${data}`);
+// Parse the IMAP greeting message
+socket.once("data", (data: Buffer) => {
+  const greeting = data.toString("utf-8").split(" ")[1] as GreetResponse;
+
+  switch (greeting) {
+    case "OK":
+      console.log("OK procees to auth -> ", data.toString("utf-8"));
+      break;
+    case "PREAUTH":
+      console.log("PREAUTH already authenticated -> ", data.toString("utf-8"));
+      break;
+    case "BYE":
+      console.log("BAD server closing -> ", data.toString("utf-8"));
+      break;
+    default:
+      socket.end();
+      return;
+  }
+
+  parseImapConnectionMessages();
 });
+
+const parseImapConnectionMessages = () =>
+  socket.on("data", (data: Buffer) => {
+    const txt = data.toString("utf-8");
+    const lines = txt.split("\r\n");
+
+    for (const line of lines) {
+      console.log(line);
+    }
+  });
 
 socket.on("end", () => {
   console.log("Close imap connection");
